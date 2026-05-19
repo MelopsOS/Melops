36d545f913c3b246c74b8806836913910f6a1231
Melops <ejostech@gmail.com>
2026-05-19 19:19:41 +0200

    Add files via upload

---
+++ melops.asm
@@ -0,0 +1,28 @@
+section .data
+    msg db "Github: MelopsOS",10
+    msg_len equ $ - msg
+
+section .text
+global _start
+
+_start:
+
+mov rax,1
+mov rdi,1
+mov rsi,msg
+mov rdx,msg_len
+syscall
+
+mov rax,60
+xor rdi,rdi
+syscall
+
+
+
+
+
+
+
+
+
+
